5b3b3199d193d393ecc1702615aee4d983023941
José Ulombe <jose.ulombe@hotmail.com>
2020-09-19 13:39:18 +0100

    Add file

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
+// create server
+
 const express = require('express')
 const nunjucks = require('nunjucks')
 
-const server = express
+const server = express()
+
+
+
+
+
+
+
+
+
+//starting server
+server.listen(3000, function() {
+    console.log("server is running")
+})
