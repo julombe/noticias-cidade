c2d018be1464a0fbdc9cd8e2d13464ee4c830db9
José Ulombe <jose.ulombe@hotmail.com>
2020-12-01 12:54:03 +0100

    add routes

--- routes.js
+++ routes.js
@@ -2,7 +2,13 @@ const express = require("express")
 const routes = express.Router()
 
 //create routes
+routes.get("/", function(req, res) {
+    return res.render("index")
+})
 
+routes.get("/brasil", function(req, res) {
+    return res.render("brasil")
+})
 
 
 
